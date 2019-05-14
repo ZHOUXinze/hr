1c40db60c5038f84a84be0967045293ab13b736a
gitPuppyZ <zhouxinze9607@outlook.com>
2019-05-14 09:07:45 +0800

    update properties

--- src/main/java/com/manage/hr/HrApplication.java
+++ src/main/java/com/manage/hr/HrApplication.java
@@ -1,9 +1,11 @@
 package com.manage.hr;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.manage.hr.dao")
 public class HrApplication {
 
     public static void main(String[] args) {
