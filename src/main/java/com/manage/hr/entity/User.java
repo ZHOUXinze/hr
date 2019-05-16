f0b74238ec46b46f6e1be15afed241d72855e69b
miao6661 <1135968516@qq.com>
2019-05-16 13:30:46 +0800

    User实体类

--- src/main/java/com/manage/hr/entity/User.java
+++ src/main/java/com/manage/hr/entity/User.java
@@ -1,14 +1,29 @@
 package com.manage.hr.entity;
 
+import javax.validation.constraints.NotEmpty;
+
 public class User {
     private Integer id;
+    @NotEmpty(message = "用户编码不能为空")
     private String userCode;
+    @NotEmpty(message = "用户名称不能为空")
     private String userName;
     private Integer userRole;
     private Integer depId;
+    @NotEmpty(message = "密码名称不能为空")
     private String password;
+    @NotEmpty(message = "电话号码名称不能为空")
     private String phoneNumber;
     private String depName;
+    private String dataName;
+
+    public String getDataName() {
+        return dataName;
+    }
+
+    public void setDataName(String dataName) {
+        this.dataName = dataName;
+    }
 
     public String getDepName() {
         return depName;
